f1bc900eb3c80e4ce6d295bd92e8c6c638d81979
Marko Vukovic <mvkvc.git@gmail.com>
2023-04-28 23:25:45 -0400

    Added recursive base params

--- lib/hydra.ex
+++ lib/hydra.ex
@@ -3,21 +3,21 @@ defmodule Hydra do
   The goal of hydra will be to read your configs an create a structured hierarchy that is built based on your selection. My assumption is that most training scripts will take place in a .exs or Livebook file, so the output should be config dict that will be used for training etc..
   """
   @config_folder "examples/yaml/conf"
-  @config_name "config.yaml"
+  @config_name "config"
 
   def run do
     load_config(@config_folder, @config_name)
   end
 
-  def load_yaml(folder, filename) do
+  def load_yaml(folder, filename, ext \\ ".yaml") do
     Application.start(:yamerl)
-    path = folder <> "/" <> filename
+    path = folder <> "/" <> filename <> ext
     IO.inspect(path)
 
     base = :yamerl_constr.file(path)
   end
 
-  def load_config(folder, filename, ext \\ ".yaml") do
+  def load_config(folder, filename) do
     base =
       load_yaml(folder, filename)
       |> List.first()
@@ -33,23 +33,70 @@ defmodule Hydra do
 
     defaults =
       Enum.reduce(default_lookup, %{}, fn x, acc ->
-        key = elem(x, 0) |> IO.inspect()
-        value = elem(x, 1) |> IO.inspect()
-        path = to_string(key) <> "/" <> to_string(value) <> ext
+        key = elem(x, 0) |> to_string()
+        value = elem(x, 1) |> to_string()
+        path = key <> "/" <> value
 
-        result =
-          load_yaml(folder, path)
-          |> List.first()
-          |> convert()
+        {key, value, path} |> IO.inspect(label: "defaults values")
 
-        Map.put(acc, key, result)
+        # result =
+        #   load_yaml(folder, path)
+        #   |> List.first()
+        #   |> convert()
+
+        # IO.inspect({key, value, result}, label: "input to base builder")
+        IO.inspect({@config_folder, key, value}, label: "bb folder, key, val")
+        result = base_builder(@config_folder, key, value)
+
+        # IO.inspect()
+        Map.put(acc, to_charlist(key), result)
       end)
 
-    # {defaults, overrides}
+    # defaults
+    {defaults, overrides} |> IO.inspect(label: "defaults, overrides")
     recursive_merge(defaults, overrides)
   end
 
-  defp recursive_merge(x, y) do
+  def base_builder(root, folder, file) do
+    base_builder(root, folder, file, [])
+  end
+
+  def base_builder(root, folder, file, params) do
+    {root, folder, file} |> IO.inspect(label: "bb inputs")
+
+    file =
+      load_yaml(root <> "/" <> folder, file)
+      |> List.first()
+      |> convert()
+
+    file |> IO.inspect(label: "base_builders file")
+
+    result =
+      if file['base'] do
+        new_file = file['base'] |> to_string()
+        {_, file} = Map.pop!(file, 'base')
+        base_builder(root, folder, new_file, params ++ [file])
+      else
+        params = params ++ [file]
+
+        Enum.reduce(params, %{}, fn x, acc ->
+          params ++ [file]
+          IO.inspect({params, acc, x}, label: "resumts merger inspect")
+          recursive_merge(acc, x)
+        end)
+      end
+
+    result |> IO.inspect(label: "base_builders result")
+    result
+  end
+
+  # def build_maps(maps) do
+  #   Enum.reduce(maps, %{}, fn x, acc ->
+  #     recursive_merge(acc, x)
+  #   end)
+  # end
+
+  def recursive_merge(x, y) do
     Map.merge(x, y, fn _k, v1, v2 ->
       if is_map(v1) and is_map(v2) do
         recursive_merge(v1, v2)
@@ -59,7 +106,7 @@ defmodule Hydra do
     end)
   end
 
-  defp convert(collection) do
+  def convert(collection) do
     IO.inspect(collection, label: "CONVERT CALLED")
 
     result =
